fe31b34be3e90d1b6c3c7f4986aace09dab7e26e
favourphilic <favourphilic@gmail.com>
2024-01-27 09:21:12 +0100

    train updated with exp name

--- train.py
+++ train.py
@@ -16,7 +16,9 @@ from sklearn.model_selection import train_test_split
 import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
-#mlflow.set_tracking_uri(uri="http://<host>:<port>")
+mlflow.set_tracking_uri(uri="https://dagshub.com/favourphilic/mlflowtracking_with_dagshub.mlflow")
+# Create a new MLflow Experiment
+mlflow.set_experiment("MLflow TrackingQuickstart")
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
